7f11afce5d0980858332d35dd387c14326bce168
sald19 <carl.sald19@gmail.com>
2022-06-15 09:14:31 -0600

    initial commit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Jetstream/Welcome.vue';
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+       test
+    </AppLayout>
+</template>
