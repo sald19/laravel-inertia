6ff5e8ce1aab6005814fb0e5b0b2c259e2c39b11
sald19 <carl.sald19@gmail.com>
2022-06-21 15:43:05 -0600

    add font-awesome icons to proyect

---
+++ resources/js/icons.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faBold,
+  faHeading,
+  faItalic,
+  faStrikethrough,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBold, faItalic, faStrikethrough, faHeading)
