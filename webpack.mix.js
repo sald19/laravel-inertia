57a4c8f908a61e617b6e9859fd5e0a5c3a47e19a
sald19 <carl.sald19@gmail.com>
2022-06-21 10:11:04 -0600

    eslint code

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
 /*
  |--------------------------------------------------------------------------
@@ -11,14 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-    ])
-    .alias({
-        '@': 'resources/js',
-    });
+mix
+  .js('resources/js/app.js', 'public/js')
+  .vue()
+  .postCss('resources/css/app.css', 'public/css', [require('tailwindcss')])
+  .alias({
+    '@': 'resources/js',
+  })
 
 if (mix.inProduction()) {
-    mix.version();
+  mix.version()
 }
