9f293f28f9fcbc42a8a29f10fdfd4de48084439b
sald19 <carl.sald19@gmail.com>
2022-06-22 09:18:25 -0600

    add icon quotes

--- resources/js/icons.js
+++ resources/js/icons.js
@@ -6,6 +6,7 @@ import {
   faItalic,
   faListOl,
   faListUl,
+  faQuoteRight,
   faStrikethrough,
 } from '@fortawesome/free-solid-svg-icons'
 
@@ -16,5 +17,6 @@ library.add(
   faItalic,
   faListOl,
   faListUl,
+  faQuoteRight,
   faStrikethrough
 )
