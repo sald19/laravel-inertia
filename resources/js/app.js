a344e01c9a579f3fce1e1d283c128147d7a023ed
sald19 <carl.sald19@gmail.com>
2022-06-24 16:08:54 -0600

    get auth user in app

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,28 +7,31 @@ import { InertiaProgress } from '@inertiajs/progress'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Bugsnag from '@bugsnag/js'
 import BugsnagPluginVue from '@bugsnag/plugin-vue'
+import { usePage } from '@inertiajs/inertia-vue3'
 
 const appName =
   window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
+
+Bugsnag.start({
+  apiKey: process.env.MIX_BUGSNAG_API_KEY,
+  appVersion: process.env.MIX_BUGSNAG_APP_VERSION,
+  plugins: [new BugsnagPluginVue()],
+  onError: (event) => {
+    console.log('usePage().props', usePage().props)
+
+    const user = usePage().props.value.auth.user
+    if (user) {
+      event.setUser(user.id, user.email, user.name)
+    }
+  },
+})
+
+const bugsnagVue = Bugsnag.getPlugin('vue')
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
   resolve: (name) => require(`./Pages/${name}.vue`),
   setup({ el, app, props, plugin }) {
-    Bugsnag.start({
-      apiKey: process.env.MIX_BUGSNAG_API_KEY,
-      appVersion: process.env.MIX_BUGSNAG_APP_VERSION,
-      plugins: [new BugsnagPluginVue()],
-      onError: (event) => {
-        const user = props.initialPage.props.auth.user
-        if (user) {
-          event.setUser(user.id, user.email, user.name)
-        }
-      },
-    })
-
-    const bugsnagVue = Bugsnag.getPlugin('vue')
-
     const vueApp = createApp({ render: () => h(app, props) })
       .use(plugin)
       .use(bugsnagVue)
