2f30b2acf76903ef983b96d889ce660bc5f78744
sald19 <carl.sald19@gmail.com>
2022-06-29 16:00:18 -0600

    change laravel mix to vite for compile assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import './bootstrap'
 import './icons'
 
 import { createApp, h } from 'vue'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -13,8 +14,8 @@ const appName =
   window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel'
 
 Bugsnag.start({
-  apiKey: process.env.MIX_BUGSNAG_API_KEY,
-  appVersion: process.env.MIX_BUGSNAG_APP_VERSION,
+  apiKey: import.meta.env.VITE_BUGSNAG_API_KEY,
+  appVersion: import.meta.env.VITE_BUGSNAG_APP_VERSION,
   plugins: [new BugsnagPluginVue()],
   onError: (event) => {
     console.log(event)
@@ -31,7 +32,11 @@ const bugsnagVue = Bugsnag.getPlugin('vue')
 
 createInertiaApp({
   title: (title) => `${title} - ${appName}`,
-  resolve: (name) => require(`./Pages/${name}.vue`),
+  resolve: (name) =>
+    resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob('./Pages/**/*.vue')
+    ),
   setup({ el, app, props, plugin }) {
     const vueApp = createApp({ render: () => h(app, props) })
       .use(plugin)
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,12 +9,12 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
