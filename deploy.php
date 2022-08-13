28e8f3d995d608519e4b8af0198373ed868ef040
sald19 <carl.sald19@gmail.com>
2022-08-13 12:15:17 -0600

    change hostname

--- deploy.php
+++ deploy.php
@@ -7,14 +7,13 @@ require 'recipe/laravel.php';
 // Config
 
 set('repository', 'git@github.com:sald19/laravel-inertia.git');
-set('update_code_strategy', 'archive');
 
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);
 
 // Hosts
-host('laravel-inertia')
+host('157.245.255.11')
     ->set('remote_user', 'deployer')
     ->set('deploy_path', '~/apps');
 
