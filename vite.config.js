f46f9c33c33b403cb04cf56c1a1b8e7963d61a20
sald19 <carl.sald19@gmail.com>
2022-08-16 09:31:49 -0600

    add vite bugsnap sourcemap package

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
-// const { BugsnagSourceMapUploaderPlugin } = require('webpack-bugsnag-plugins')
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+import { BugsnagSourceMapUploaderPlugin } from 'vite-plugin-bugsnag'
 
 export default defineConfig((command, mode) => {
-  // const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [
@@ -29,17 +29,11 @@ export default defineConfig((command, mode) => {
         },
       }),
 
-      // new CleanWebpackPlugin({
-      //   cleanStaleWebpackAssets: false,
-      //   protectWebpackAssets: true,
-      //   cleanOnceBeforeBuildPatterns: ['js/components/*'],
-      // }),
-      // new BugsnagSourceMapUploaderPlugin({
-      //   apiKey: env.VITE_BUGSNAG_API_KEY,
-      //   appVersion: env.VITE_BUGSNAG_APP_VERSION,
-      //   publicPath: '*',
-      //   overwrite: true,
-      // }),
+      BugsnagSourceMapUploaderPlugin({
+        apiKey: env.VITE_BUGSNAG_API_KEY,
+        appVersion: env.VITE_BUGSNAG_APP_VERSION,
+        overwrite: true,
+      }),
     ],
 
     resolve: {
