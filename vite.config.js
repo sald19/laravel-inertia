c591af5be9e67b34bb4f0abc149d1d00f3dcbdb2
sald19 <carl.sald19@gmail.com>
2022-08-20 14:40:46 -0600

    #924 send changes

--- vite.config.js
+++ vite.config.js
@@ -42,10 +42,10 @@ export default defineConfig((command, mode) => {
       },
     },
 
-    devtool: 'hidden-source-map',
+    // devtool: 'hidden-source-map',
 
-    output: {
-      chunkFilename: 'js/components/[name].js?id=[contenthash]',
-    },
+    // output: {
+    //   chunkFilename: 'js/components/[name].js?id=[contenthash]',
+    // },
   }
 })
