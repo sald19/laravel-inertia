5f91d3e0b62a623079fae0974639328d630d4e9d
sald19 <carl.sald19@gmail.com>
2023-04-04 08:54:32 -0600

    apply rule Declare Strict Types

--- app/Console/Commands/Search/SetupSearchFilters.php
+++ app/Console/Commands/Search/SetupSearchFilters.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands\Search;
 
 use App\Models\Post;
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PostController;
 use App\Models\User;
