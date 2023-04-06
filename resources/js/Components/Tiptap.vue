ec56dfe8dda81459b87f89708242ed92df14e0ce
sald19 <carl.sald19@gmail.com>
2023-04-06 14:42:31 -0600

    configurate hocuspocus provider

--- resources/js/Components/Tiptap.vue
+++ resources/js/Components/Tiptap.vue
@@ -1,5 +1,12 @@
 <script setup>
-import { useEditor, EditorContent } from '@tiptap/vue-3'
+import { computed } from 'vue'
+import { EditorContent, useEditor } from '@tiptap/vue-3'
+import { usePage } from '@inertiajs/vue3'
+import * as Y from 'yjs'
+
+import { HocuspocusProvider } from '@hocuspocus/provider'
+import Collaboration from '@tiptap/extension-collaboration'
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
 import Highlight from '@tiptap/extension-highlight'
 import StarterKit from '@tiptap/starter-kit'
 
@@ -9,20 +16,51 @@ const props = defineProps({
   modelValue: Object,
 })
 
-const emit = defineEmits(['update:modelValue'])
+const ydoc = new Y.Doc()
+const colors = [
+  '#958DF1',
+  '#F98181',
+  '#FBBC88',
+  '#FAF594',
+  '#70CFF8',
+  '#94FADB',
+  '#B9F18D',
+]
+
+const user = computed(() => usePage().props.auth.user)
+
+const randomColor = computed(() => {
+  return colors[Math.floor(Math.random() * colors.length)]
+})
+
+const provider = new HocuspocusProvider({
+  url: `ws://127.0.0.1:1234/collaboration/16`,
+  name: 'furulla',
+  document: ydoc,
+})
 
 const editor = useEditor({
-  content: props.modelValue,
   editorProps: {
     attributes: {
       class:
         'prose prose-sm sm:prose focus:outline-none min-h-full w-full min-w-full p-2',
     },
   },
-  extensions: [Highlight, StarterKit],
-  onUpdate: ({ editor }) => {
-    emit('update:modelValue', editor.getJSON())
-  },
+  extensions: [
+    Highlight,
+    StarterKit.configure({ history: false }),
+    Collaboration.configure({
+      document: ydoc,
+    }),
+    CollaborationCursor.configure({
+      provider,
+      user: {
+        id: user.value.id,
+        name: user.value.name,
+        color: randomColor.value,
+      },
+    }),
+  ],
 })
 </script>
 
@@ -30,7 +68,43 @@ const editor = useEditor({
   <div>
     <div v-if="editor" class="border border-gray-300 bg-white rounded">
       <menu-bar :editor="editor" />
-      <editor-content :editor="editor" class="min-h-24" />
+      <editor-content :editor="editor" class="ProseMirror min-h-24" />
     </div>
   </div>
 </template>
+
+<style scoped>
+.ProseMirror ::v-deep(p.is-editor-empty:first-child::before) {
+  content: attr(data-placeholder);
+  float: left;
+  color: #adb5bd;
+  pointer-events: none;
+  height: 0;
+}
+
+/* Give a remote user a caret */
+.ProseMirror ::v-deep(.collaboration-cursor__caret) {
+  position: relative;
+  margin-left: -1px;
+  margin-right: -1px;
+  border-left: 1px solid #0d0d0d;
+  border-right: 1px solid #0d0d0d;
+  word-break: normal;
+  pointer-events: none;
+}
+
+.ProseMirror ::v-deep(.collaboration-cursor__label) {
+  position: absolute;
+  top: -1.4em;
+  left: -1px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  user-select: none;
+  color: #0d0d0d;
+  padding: 0.1rem 0.3rem;
+  border-radius: 3px 3px 3px 0;
+  white-space: nowrap;
+}
+</style>
