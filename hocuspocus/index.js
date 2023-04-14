06da83264b297701c52779c5ecba60059d0c19ec
sald19 <carl.sald19@gmail.com>
2023-04-14 15:11:36 -0600

    test hocuspocus

--- hocuspocus/index.js
+++ hocuspocus/index.js
@@ -5,9 +5,9 @@ import server from './server/index.js'
 
 const { app } = expressWebsockets(express())
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
+app.get('/', (request, response) => {
+  response.send('Hello World!')
+})
 
 app.ws('/collaboration/:document', (websocket, request) => {
   const context = {
@@ -20,4 +20,4 @@ app.ws('/collaboration/:document', (websocket, request) => {
   server.handleConnection(websocket, request, request.params.document, context)
 })
 
-app.listen(1234, () => console.log('Listening on http://127.0.0.1:1234'))
+app.listen(6001, () => console.log('Listening on http://127.0.0.1:1234'))
