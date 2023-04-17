05a20424959eaa3ddb1eb0567f237bbb631974eb
sald19 <carl.sald19@gmail.com>
2023-04-17 11:33:50 -0600

    add task restart hocuspocus with supervisor

--- deploy.php
+++ deploy.php
@@ -38,7 +38,7 @@ task('prisma:generate', function () {
 });
 
 task('supervisor:restart', function () {
-    run('sudo -s supervisorctl status');
+    run('sudo -S supervisorctl status');
 })->desc('Restart supervisord restart');
 
 after('deploy:shared', 'hocuspocus:install');
