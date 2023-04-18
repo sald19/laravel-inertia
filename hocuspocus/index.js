e8eb9ae0c48fbf5aca264051023dbcf6832a871e
sald19 <carl.sald19@gmail.com>
2023-04-18 15:56:01 -0600

    change log port

--- hocuspocus/index.js
+++ hocuspocus/index.js
@@ -20,4 +20,4 @@ app.ws('/collaboration', (websocket, request) => {
   server.handleConnection(websocket, request, request.params.document, context)
 })
 
-app.listen(6001, () => console.log('Listening on http://127.0.0.1:1234'))
+app.listen(6001, () => console.log('Listening on http://127.0.0.1:6001'))
